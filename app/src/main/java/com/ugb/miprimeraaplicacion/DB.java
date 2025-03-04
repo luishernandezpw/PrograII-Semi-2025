11962333a1e64ff0df8df74346e2fa6788baede2
Luis Hernandez <Estudiante@DESKTOP-J6D5H3A>
2025-03-04 10:27:40 -0600

    Se propgramo el poder guardar registros de amigos y listar los amigos

--- app/src/main/java/com/ugb/miprimeraaplicacion/DB.java
+++ app/src/main/java/com/ugb/miprimeraaplicacion/DB.java
@@ -1,6 +1,7 @@
 package com.ugb.miprimeraaplicacion;
 
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
@@ -41,5 +42,9 @@ public class DB extends SQLiteOpenHelper {
         } catch (Exception e) {
             return e.getMessage();
         }
+    }
+    public Cursor lista_amigos() {
+        SQLiteDatabase db = getReadableDatabase();
+        return db.rawQuery("SELECT * FROM amigos", null);
     }
 }
