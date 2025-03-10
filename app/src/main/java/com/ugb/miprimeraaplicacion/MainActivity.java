e7df720c1a491cc2ec8c201bbd37d7392e7fad9d
Luis Hernandez <sarco_luis@hotmail.com>
2025-03-10 08:30:51 -0600

    Continuamos con las operaciones del CRUD de amigos

--- app/src/main/java/com/ugb/miprimeraaplicacion/MainActivity.java
+++ app/src/main/java/com/ugb/miprimeraaplicacion/MainActivity.java
@@ -1,6 +1,9 @@
 package com.ugb.miprimeraaplicacion;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -8,7 +11,10 @@ import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
 public class MainActivity extends AppCompatActivity {
+    FloatingActionButton fab;
     Button btn;
     TextView tempVal;
     DB db;
@@ -19,12 +25,14 @@ public class MainActivity extends AppCompatActivity {
 
         db = new DB(this);
         btn = findViewById(R.id.btnGuardarAmigo);
-        btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                guardarAmigo();
-            }
-        });
+        btn.setOnClickListener(view->guardarAmigo());
+
+        fab = findViewById(R.id.fabListaAmigos);
+        fab.setOnClickListener(view->abrirVentana());
+    }
+    private void abrirVentana(){
+        Intent intent = new Intent(this, lista_amigos.class);
+        startActivity(intent);
     }
     private void guardarAmigo() {
         tempVal = findViewById(R.id.txtNombre);
@@ -44,6 +52,6 @@ public class MainActivity extends AppCompatActivity {
         String[] datos = {"", nombre, direccion, telefono, email, dui, ""};
         db.administrar_amigos("agregar", datos);
         Toast.makeText(getApplicationContext(), "Registro guardado con exito.", Toast.LENGTH_LONG).show();
+        abrirVentana();
     }
-
 }
