ba3b9d4cbd236c2236e046edaf58228160e2fb2a
Luis Hernandez <Estudiante@DESKTOP-J6D5H3A>
2025-04-01 10:01:38 -0600

    terminacion de la agenda amigos, eliminacion

--- app/src/main/java/com/ugb/miprimeraaplicacion/lista_amigos.java
+++ app/src/main/java/com/ugb/miprimeraaplicacion/lista_amigos.java
@@ -96,6 +96,22 @@ public class lista_amigos extends Activity {
             confirmacion.setMessage(nombre);
             confirmacion.setPositiveButton("Si", (dialog, which) -> {
                 try {
+                    di = new detectarInternet(this);
+                    if(di.hayConexionInternet()){//online
+                        JSONObject datosAmigos = new JSONObject();
+                        String _id = jsonArray.getJSONObject(posicion).getJSONObject("value").getString("_id");
+                        String _rev = jsonArray.getJSONObject(posicion).getJSONObject("value").getString("_rev");
+                        String url = utilidades.url_mto + "/" + _id + "?rev=" + _rev;
+                        enviarDatosServidor objEnviarDatosServidor = new enviarDatosServidor(this);
+                        String respuesta = objEnviarDatosServidor.execute(datosAmigos.toString(), "DELETE", url).get();
+                        JSONObject respuestaJSON = new JSONObject(respuesta);
+                        if(respuestaJSON.getBoolean("ok")) {
+                            obtenerDatosAmigos();
+                            mostrarMsg("Registro eliminado con exito");
+                        }else{
+                            mostrarMsg("Error: " + respuesta);
+                        }
+                    }
                     String respuesta = db.administrar_amigos("eliminar", new String[]{jsonArray.getJSONObject(posicion).getJSONObject("value").getString("idAmigo")});
                     if(respuesta.equals("ok")) {
                         obtenerDatosAmigos();
