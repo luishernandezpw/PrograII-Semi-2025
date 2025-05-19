c156699d9e12c4561edd0569f24baf0a569e08ee
Luis Hernandez <sarco_luis@hotmail.com>
2025-05-19 08:54:53 -0600

    Continuacion del desarrollo de las notificaciones push

---
+++ app/src/main/java/com/ugb/miprimeraaplicacion/chats.java
@@ -0,0 +1,12 @@
+package com.ugb.miprimeraaplicacion;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class chats extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.chats);
+    }
+}
